3a39a1ecac89ec51299c183e4cb13c9e64641e27
Jyotirmaya Mahanta <jyotirmaya.mahanta@gmail.com>
2024-04-12 23:25:39 +0530

    hellogoodbye

---
+++ HelloGoodbye.java
@@ -0,0 +1,6 @@
+public class HelloGoodbye {
+
+    public static void main(String[] args) {
+        System.out.println("Hello " + args[0] + " and " + args[1] + ".\nGoodbye " + args[1] + " and " + args[0] + ".");
+    }
+}
