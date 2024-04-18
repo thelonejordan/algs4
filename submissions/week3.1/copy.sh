97041199b2a78b2d0e094ad6d42f49f3539c4ea0
Jyotirmaya Mahanta <jyotirmaya.mahanta@gmail.com>
2024-04-18 19:46:11 +0530

    collinear points skeleton added

---
+++ submissions/week3.1/copy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+rm BruteCollinearPoints.java
+rm FastCollinearPoints.java
+rm Point.java
+cp ../../collection/BruteCollinearPoints.java .
+cp ../../collection/FastCollinearPoints.java .
+cp ../../collection/Point.java .
---
+++ submissions/week3.1/zipper.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+rm collinear.zip
+zip collinear.zip BruteCollinearPoints.java FastCollinearPoints.java Point.java
+cp collinear.zip ~/Downloads
